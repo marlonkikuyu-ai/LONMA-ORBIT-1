ce917ebb105e5c571742f16a3e9f54ba46e87bbd
marlonkikuyu-ai <marlonkikuyu@gmail.com>
2026-08-17 20:57:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from database import init_db
 from modules.auth import router as auth_router
-from modules.user import router as user_router
 
 app = FastAPI(title="LONMA Orbit API")
 
@@ -10,4 +9,3 @@ def on_startup():
     init_db()
 
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
-app.include_router(user_router, prefix="/user", tags=["User"])
