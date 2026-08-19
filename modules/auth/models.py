b89f917da3ffbf5cefe70701659a044c7a176925
marlonkikuyu-ai <marlonkikuyu@gmail.com>
2026-08-19 22:25:00 +0300

    Update models.py

--- modules/auth/models.py
+++ modules/auth/models.py
@@ -7,7 +7,6 @@ from database import Base
 class User(Base):
     __tablename__ = "users"
     __table_args__ = {'extend_existing': True}
-    
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
